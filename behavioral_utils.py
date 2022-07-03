c983f1dc23fd023ef02115c3e0ce2ab90cbe28b0
Patrick Zhang <pqz317@uw.edu>
2022-07-03 16:17:49 -0700

    add grabbing selection features

--- behavioral_utils.py
+++ behavioral_utils.py
@@ -14,3 +14,18 @@ def get_trial_intervals(behavioral_data, event="FeedbackOnset", pre_interval=0, 
     intervals[:, 2] = trial_event_times[event] + post_interval
     intervals_df = pd.DataFrame(intervals, columns=["TrialNumber", "IntervalStartTime", "IntervalEndTime"])
     return intervals_df
+
+def get_selection_features(behavioral_data):
+    """
+    Per trial, gets the Color, Shape Pattern of the selected card
+    Returns: DataFrame with TrialNumber, Color, Pattern, Shape columns
+    """
+    # columns: TrialNumber, Color, Shape, Pattern
+    selections = []
+    for _, row in behavioral_data.iterrows():
+        item_chosen = int(row["ItemChosen"])
+        color = row[f"Item{item_chosen}Color"]
+        shape = row[f"Item{item_chosen}Shape"]
+        pattern = row[f"Item{item_chosen}Pattern"]
+        selections.append([row["TrialNumber"], color, shape, pattern])
+    return pd.DataFrame(selections, columns=["TrialNumber", "Color", "Shape", "Pattern"])
