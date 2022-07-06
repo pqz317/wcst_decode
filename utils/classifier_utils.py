97c023a2dd371557eea4674ad72ee31c582dc404
Patrick Zhang <pqz317@uw.edu>
2022-07-05 17:43:15 -0700

    have some classifiers working

--- utils/classifier_utils.py
+++ utils/classifier_utils.py
@@ -1,3 +1,64 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
 
-# have a util for 
-def evaluate_model(model, partition_func)
+def transform_to_input_data(firing_rates, trials_filter=None):
+    """
+    Transform DataFrame with columns TrialNumber, UnitID, TimeBins, Value
+    to a num_trials x num_inputs numpy array for classifier input
+    """
+    df = firing_rates
+    if trials_filter is not None: 
+        df = df[df["TrialNumber"].isin(trials_filter)]
+    num_time_bins = len(df["TimeBins"].unique())
+    num_units = len(df["UnitID"].unique())
+    num_trials = len(df["TrialNumber"].unique())
+    num_inputs = num_time_bins * num_units
+    sorted = df.sort_values(by=["TrialNumber"])
+    inputs = sorted["Value"].to_numpy().reshape((num_trials, num_inputs))
+    return inputs
+
+
+def transform_to_label_data(feature_selections, trials_filter=None):
+    df = feature_selections
+    if trials_filter is not None:
+        df = df[df["TrialNumber"].isin(trials_filter)]
+    sorted = df.sort_values(by=["TrialNumber"])
+    inputs = sorted["Feature"].to_numpy()
+    return inputs
+
+
+# classsifier utils
+def evaluate_classifier(clf, firing_rates, feature_selections, num_runs=20):
+    test_accs = []
+    train_accs = []
+    shuffled_accs = []
+    models = []
+    trials = feature_selections["TrialNumber"].unique()
+    for i in range(num_runs):
+        print(f"Run {i}")
+        train_trials, test_trials = train_test_split(trials, test_size=0.2)
+        X_train = transform_to_input_data(firing_rates, trials_filter=train_trials)
+        y_train = transform_to_label_data(feature_selections, trials_filter=train_trials)
+
+        X_test = transform_to_input_data(firing_rates, trials_filter=test_trials)
+        y_test = transform_to_label_data(feature_selections, trials_filter=test_trials)
+        clf = clf.fit(X_train, y_train)
+        # train_pred = clf.predict(X_train)
+        train_acc = clf.score(X_train, y_train)
+
+        # test_pred = clf.predict(X_test)
+        test_acc = clf.score(X_test, y_test)
+
+        train_accs.append(train_acc)
+        test_accs.append(test_acc)
+        
+
+        y_test_shuffle = y_test
+        np.random.shuffle(y_test_shuffle)
+        # shuffled_acc = sum(test_pred==y_test_shuffle)/len(y_test_shuffle)
+        shuffled_acc = clf.score(X_test, y_test_shuffle)
+        shuffled_accs.append(shuffled_acc)
+        
+        models.append(clf)
+        
+    return train_accs, test_accs, shuffled_accs, models
