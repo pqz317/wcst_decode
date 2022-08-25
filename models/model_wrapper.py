b6efb717d96291f748aa2688930b4ece86b24fa3
Patrick Zhang <pqz317@uw.edu>
2022-08-25 14:20:42 -0700

    pytorch working

--- models/model_wrapper.py
+++ models/model_wrapper.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 class ModelWrapper:
     def __init__(self, model, trainer, labels):
@@ -8,17 +9,20 @@ class ModelWrapper:
         self.idx_to_labels = {idx: label for idx, label in enumerate(labels)}
 
     def fit(self, x_train, y_train):
-        y_train_idxs = np.array([self.labels_to_idx[label] for label in y_train.tolist()])
+        y_train_idxs = np.array([self.labels_to_idx[label] for label in y_train.tolist()]).astype(int)
         self.trainer.train(self.model, x_train, y_train_idxs)
+        return self
 
     def predict(self, x_test):
-        probs = self.model.forward(x_test)
-        label_idxs = np.amax(probs, axis=1)
+        x_test =  torch.Tensor(x_test)
+        probs = self.model(x_test)
+        label_idxs = np.argmax(probs.detach().numpy(), axis=1)
         return np.array([self.idx_to_labels[label_idxs] for label_idxs in label_idxs.tolist()])
 
     def score(self, x_test, y_test):
         labels = self.predict(x_test)
-        return np.sum(labels == y_test) / len(y_test)
+        score = np.sum(labels == y_test) / len(y_test)
+        return score
 
     @property
     def _coef(self):
--- models/trainer.py
+++ models/trainer.py
@@ -1,11 +1,15 @@
+import torch
+from torch import nn
+
 class Trainer:
-    def __init__(learning_rate=0.001, max_iter=1000):
+    def __init__(self, learning_rate=0.001, max_iter=1000):
         self.learning_rate = learning_rate
         self.max_iter = max_iter
         pass
 
-    def train(model, x_train, y_train):
-        x_train, y_train = torch.Tensor(x_train),torch.Tensor(y_train)
+    def train(self, model, x_train, y_train):
+        x_train =  torch.Tensor(x_train)
+        y_train = torch.Tensor(y_train).to(torch.long)
 
         device = "cuda" if torch.cuda.is_available() else "cpu"
         criterion = nn.CrossEntropyLoss().to(device)
