25de02400fc33421755f22a31691278eb27d1c75
Patrick Zhang <pqz317@uw.edu>
2023-11-13 22:35:43 +0000

    check glm res, debug

--- scripts/glm_analysis/hyak_combined_shuffles_glm.py
+++ scripts/glm_analysis/hyak_combined_shuffles_glm.py
@@ -2,23 +2,29 @@ import utils.behavioral_utils as behavioral_utils
 import utils.information_utils as information_utils
 import utils.io_utils as io_utils
 import pandas as pd
+import argparse
 
 SESSIONS_PATH = "/data/valid_sessions_rpe.pickle"
 NUM_SHUFFLES = 1000
 
-def aggregate_shuffles(sess_name):
+def aggregate_shuffles(sess_name, shuffle_type):
     print(f"Processing {sess_name}")
     shuffles = []
     for i in range(NUM_SHUFFLES):
-        shuffle = pd.read_pickle(f"/data/res/{sess_name}_glm_card_shuffle_{i}.pickle")
+        shuffle = pd.read_pickle(f"/data/res/{sess_name}_glm_{shuffle_type}_shuffle_{i}.pickle")
         shuffle["ShuffleIdx"] = i
         shuffles.append(shuffle)
     shuffles = pd.concat(shuffles)
-    shuffles.to_pickle(f"/data/res/{sess_name}_glm_card_shuffles.pickle")
+    shuffles.to_pickle(f"/data/res/{sess_name}_glm_{shuffle_type}_shuffles.pickle")
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--shuffle_type', type=str, help="type of shuffle, card or feature_rpe", default="feature_rpe")
+    args = parser.parse_args()
+    shuffle_type = args.shuffle_type
+
     valid_sess = pd.read_pickle(SESSIONS_PATH)
-    valid_sess.apply(lambda x: aggregate_shuffles(x.session_name), axis=1)
+    valid_sess.apply(lambda x: aggregate_shuffles(x.session_name, shuffle_type), axis=1)
 
 if __name__ == "__main__":
     main()
