d61257140b17fee025a24593854515bc9bf9ff35
Patrick Zhang <pqz317@uw.edu>
2024-01-27 23:26:36 +0000

    fix bug in trial splitter, make more robust

--- trial_splitters/condition_trial_splitter.py
+++ trial_splitters/condition_trial_splitter.py
@@ -11,13 +11,30 @@ class ConditionTrialSplitter:
     Per condition specified, splits trials into train/test randomly. 
     """
 
-    def __init__(self, beh_df, condition_column, test_ratio, seed=None):
+    def __init__(self, beh_df, condition_column, test_ratio, min_trials_per_cond=2, seed=None):
+        """
+        Args: 
+            beh_df: behavioral dataframe, with condition column, and TrialNumbers column
+            condition_column: column used to split on. train/test splits will be assigned per-condition
+            test_ratio: ratio of test trials to split out
+            min_trials_per_cond: minimum number of trials for each condition. 
+                Will raise an error during iteration if not satisfied. 
+        """
         # get condition -> block numbers
         self.rng = np.random.default_rng(seed=seed)
         self.beh_df = beh_df
+        self.min_trials_per_cond = min_trials_per_cond
         trials = beh_df.groupby(by=condition_column).apply(lambda g: g.TrialNumber.unique())
         self.trials_df = pd.DataFrame({"Condition": trials.index, "TrialNumbers": trials.values})
+        self._check_min_trials()
         self.test_ratio = test_ratio
+
+    def _check_min_trials(self):
+        def check_cond(row):
+            trials = list(row["TrialNumbers"]) 
+            if len(trials) < self.min_trials_per_cond:
+                raise ValueError(f"Condition {row.Condition} only has {len(trials)} trials, need {self.min_trials_per_cond}")
+        self.trials_df.apply(check_cond, axis=1)
 
     def __iter__(self):
         return self
@@ -31,9 +48,11 @@ class ConditionTrialSplitter:
                 - TestTrials: trials satisfying this condition held out for testing, as a list per row
         """
         def split_train_test(row):
-            trials = list(row["TrialNumbers"])
+            trials = list(row["TrialNumbers"])                
             self.rng.shuffle(trials)
-            split_at = int(len(trials) * self.test_ratio)
+            # round up number of test trials
+            # ex. if test ratio at 0.2, even with 2 trials, train/test split will still be 1/1. 
+            split_at = np.ceil(len(trials) * self.test_ratio)
             row["TestTrials"] = trials[:split_at]
             row["TrainTrials"] = trials[split_at:]
             return row
