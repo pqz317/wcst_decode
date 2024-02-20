88d79f2101ca499a3b9f1ca5ffcbad06a55e8f6b
Patrick Zhang <pqz317@uw.edu>
2024-02-20 05:22:35 +0000

    re-run shuffle for regular linear regression

--- constants/glm_constants.py
+++ constants/glm_constants.py
@@ -5,6 +5,6 @@ INCLUDE_PREDICTIONS = True
 EVENT = "FeedbackOnset"  # event in behavior to align on
 PRE_INTERVAL = 1300   # time in ms before event
 POST_INTERVAL = 1500  # time in ms after event
-INTERVAL_SIZE = 100  # size of interval in ms
+INTERVAL_SIZE = 50  # size of interval in ms
 NUM_BINS_SMOOTH = 1
 
