deb338eae488d1ca1b171c16b3048904a426ee29
Patrick Zhang <pqz317@uw.edu>
2024-04-05 04:28:52 +0000

    set index for value glm

--- scripts/glm_analysis/20240402_fit_value_glm.py
+++ scripts/glm_analysis/20240402_fit_value_glm.py
@@ -28,6 +28,7 @@ def calc_and_save_session(sess_name):
     beh, frs = io_utils.load_rpe_sess_beh_and_frs(sess_name, beh_path=SESS_BEHAVIOR_PATH, fr_path=SESS_SPIKES_PATH)
     # get the values
     beh = behavioral_utils.get_feature_values_per_session(sess_name, beh)
+    beh = beh.set_index(["TrialNumber"])
     columns_to_flatten = ["RPEGroup"] + FEATURE_DIMS + INTERACTIONS
     input_columns = columns_to_flatten + FEATURES
     value_reses = glm_utils.fit_glm_for_data((beh, frs), input_columns=input_columns, columns_to_flatten=columns_to_flatten)
