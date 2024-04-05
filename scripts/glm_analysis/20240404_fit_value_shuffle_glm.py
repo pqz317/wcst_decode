9a134d436516e35b4ffe62b022bdd1f4c08b8dfa
Patrick Zhang <pqz317@uw.edu>
2024-04-05 05:10:17 +0000

    val shuffle code

---
+++ scripts/glm_analysis/20240404_fit_value_shuffle_glm.py
@@ -0,0 +1,58 @@
+import argparse
+import os
+import numpy as np
+import pandas as pd
+import utils.behavioral_utils as behavioral_utils
+import utils.information_utils as information_utils
+import utils.io_utils as io_utils
+import utils.glm_utils as glm_utils
+from matplotlib import pyplot as plt
+import time
+from constants.glm_constants import *
+from constants.behavioral_constants import *
+
+# the output directory to store the data
+OUTPUT_DIR = "/data/res"
+SESSIONS_PATH = "/data/valid_sessions_rpe.pickle"
+SESS_BEHAVIOR_PATH = "/data/sub-SA_sess-{sess_name}_object_features.csv"
+# path for each session, for spikes that have been pre-aligned to event time and binned. 
+SESS_SPIKES_PATH = "/data/{sess_name}_firing_rates_{pre_interval}_{event}_{post_interval}_{interval_size}_bins_{num_bins_smooth}_smooth.pickle"
+
+
+FEATURE_DIMS = ["Color", "Shape", "Pattern"]
+INTERACTIONS = [f"{dim}RPE" for dim in FEATURE_DIMS]
+
+def calc_and_save_session(sess_name, shuffle_idx):
+    start = time.time()
+    print(f"Processing session {sess_name}")
+    beh, frs = io_utils.load_rpe_sess_beh_and_frs(sess_name, beh_path=SESS_BEHAVIOR_PATH, fr_path=SESS_SPIKES_PATH, set_indices=False)
+    # get the values
+    beh = behavioral_utils.get_feature_values_per_session(sess_name, beh)
+    beh = beh.set_index(["TrialNumber"])
+    frs = frs.set_index(["TrialNumber"])
+
+
+    rng = np.random.default_rng(shuffle_idx)
+    shuffled_beh = glm_utils.create_shuffles(beh, FEATURES, rng)
+    columns_to_flatten = ["RPEGroup"] + FEATURE_DIMS + INTERACTIONS
+    input_columns = columns_to_flatten + FEATURES
+    shuffled_res = glm_utils.fit_glm_for_data((shuffled_beh, frs), input_columns)
+
+    shuffled_res.to_pickle(os.path.join(OUTPUT_DIR, f"{sess_name}_glm_{MODE}_{INTERVAL_SIZE}_{MODEL}_value_shuffle_{shuffle_idx}.pickle"))
+
+    end = time.time()
+    print(f"Session {sess_name} took {(end - start) / 60} minutes")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('shuffle_idx', type=int, help="int from 0 - 999 denoting which session to run for")
+
+    args = parser.parse_args()
+    shuffle_idx = int(args.shuffle_idx)
+    valid_sess = pd.read_pickle(SESSIONS_PATH)
+    print(f"processing shuffle index {shuffle_idx} for all {len(valid_sess)} sessions")
+    valid_sess.apply(lambda row: calc_and_save_session(row.session_name, shuffle_idx), axis=1)
+
+if __name__ == "__main__":
+    main()
+
