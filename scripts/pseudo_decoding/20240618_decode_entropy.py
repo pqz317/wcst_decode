e1ad00743aa04ccfc8d0a62d6eebbb9aad13d906
Patrick Zhang <pqz317@uw.edu>
2024-06-18 21:48:36 +0000

    decode entropy

---
+++ scripts/pseudo_decoding/20240618_decode_entropy.py
@@ -0,0 +1,124 @@
+"""
+Decode features by dimension
+Split by condition of whether features are high valued or not
+Subselect trials where high valued feature is also chosen
+Look during simulus onset, 500ms before, 500ms after. 
+"""
+
+import os
+import numpy as np
+import pandas as pd
+import utils.pseudo_utils as pseudo_utils
+import utils.pseudo_classifier_utils as pseudo_classifier_utils
+import utils.behavioral_utils as behavioral_utils
+from constants.behavioral_constants import *
+from constants.decoding_constants import *
+from utils.session_data import SessionData
+
+from models.trainer import Trainer
+from models.model_wrapper import ModelWrapper, ModelWrapperLinearRegression
+from models.multinomial_logistic_regressor import NormedDropoutMultinomialLogisticRegressor
+from trial_splitters.condition_trial_splitter import ConditionTrialSplitter 
+
+import argparse
+
+# the output directory to store the data
+OUTPUT_DIR = "/data/res/pseudo"
+# path to a dataframe of sessions to analyze
+# SESSIONS_PATH = "/data/patrick_scratch/multi_sess/valid_sessions.pickle"
+SESSIONS_PATH = "/data/valid_sessions_rpe.pickle"
+
+# path for each session, specifying behavior
+SESS_BEHAVIOR_PATH = "/data/behavior/sub-SA_sess-{sess_name}_object_features.csv"
+# path for each session, for spikes that have been pre-aligned to event time and binned. 
+SESS_SPIKES_PATH = "/data/firing_rates/{sess_name}_firing_rates_{pre_interval}_{event}_{post_interval}_{interval_size}_bins_1_smooth.pickle"
+
+
+DATA_MODE = "FiringRate"
+EVENT = "StimOnset"  # event in behavior to align on
+PRE_INTERVAL = 1000   # time in ms before event
+POST_INTERVAL = 1000  # time in ms after event
+INTERVAL_SIZE = 100  # size of interval in ms
+# EVENT = "FeedbackOnset"  # event in behavior to align on
+# PRE_INTERVAL = 1300   # time in ms before event
+# POST_INTERVAL = 1500  # time in ms after event
+# INTERVAL_SIZE = 100  # size of interval in ms
+
+def get_feat_beh(session, feat):
+    feat_beh = behavioral_utils.get_beh_model_labels_for_session_feat(session, feat, beh_path=SESS_BEHAVIOR_PATH)
+    return feat_beh
+
+
+def load_session_data(row, condition, shuffle_idx=None):
+    sess_name = row.session
+
+    behavior_path = SESS_BEHAVIOR_PATH.format(sess_name=sess_name)
+    beh = pd.read_csv(behavior_path)
+    valid_beh = behavioral_utils.get_valid_trials(beh)
+    feature_selections = behavioral_utils.get_selection_features(valid_beh)
+    valid_beh = pd.merge(valid_beh, feature_selections, on="TrialNumber", how="inner")
+    beh = behavioral_utils.get_feature_values_per_session(sess_name, valid_beh)
+    beh = behavioral_utils.get_max_feature_value(beh)
+    beh = behavioral_utils.calc_feature_probs(beh)
+    beh = behavioral_utils.calc_feature_value_entropy(beh, num_bins=2, quantize_bins=True)
+
+    # shift TrialNumbers by some random amount
+    if shuffle_idx is not None: 
+        beh = behavioral_utils.shuffle_beh_by_shift(beh, buffer=25, seed=shuffle_idx)
+
+    spikes_path = SESS_SPIKES_PATH.format(
+        sess_name=sess_name, 
+        pre_interval=PRE_INTERVAL, 
+        event=EVENT, 
+        post_interval=POST_INTERVAL, 
+        interval_size=INTERVAL_SIZE
+    )
+    frs = pd.read_pickle(spikes_path)
+    frs = frs.rename(columns={DATA_MODE: "Value"})
+    splitter = ConditionTrialSplitter(beh, "FeatEntropyBin", TEST_RATIO, seed=DECODER_SEED)
+    session_data = SessionData(sess_name, beh, frs, splitter)
+    session_data.pre_generate_splits(NUM_SPLITS)
+    return session_data
+
+def decode(sessions, shuffle_idx):
+    sess_datas = sessions.apply(lambda row: load_session_data(row, shuffle_idx), axis=1)
+
+    # setup decoder, specify all possible label classes, number of neurons, parameters
+    classes = [0, 1]
+    num_neurons = sess_datas.apply(lambda x: x.get_num_neurons()).sum()
+    init_params = {"n_inputs": num_neurons, "p_dropout": 0.5, "n_classes": len(classes)}
+    # create a trainer object
+    trainer = Trainer(learning_rate=0.05, max_iter=500, batch_size=1000)
+    # create a wrapper for the decoder
+    model = ModelWrapper(NormedDropoutMultinomialLogisticRegressor, init_params, trainer, classes)
+
+    # calculate time bins (in seconds)
+    time_bins = np.arange(0, (POST_INTERVAL + PRE_INTERVAL) / 1000, INTERVAL_SIZE / 1000)
+    train_accs, test_accs, shuffled_accs, models = pseudo_classifier_utils.evaluate_classifiers_by_time_bins(
+        model, sess_datas, time_bins, NUM_SPLITS, NUM_TRAIN_PER_COND, NUM_TEST_PER_COND, DECODER_SEED
+    ) 
+    # store the results
+    shuffle_str = "" if shuffle_idx is None else f"shuffle_{shuffle_idx}_"
+    run_name = f"entropy_{EVENT}_{shuffle_str}"
+    np.save(os.path.join(OUTPUT_DIR, f"{run_name}train_accs.npy"), train_accs)
+    np.save(os.path.join(OUTPUT_DIR, f"{run_name}test_accs.npy"), test_accs)
+    np.save(os.path.join(OUTPUT_DIR, f"{run_name}shuffled_accs.npy"), shuffled_accs)
+    np.save(os.path.join(OUTPUT_DIR, f"{run_name}models.npy"), models)
+    # sess_datas.to_pickle(os.path.join(OUTPUT_DIR, f"{run_name}sess_datas.pickle"))
+
+def main():
+    """
+    Loads a dataframe specifying sessions to use
+    For each feature dimension, runs decoding, stores results. 
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--shuffle_idx', default=None, type=int)
+
+    args = parser.parse_args()
+    valid_sess = pd.read_pickle(SESSIONS_PATH)
+    print(f"Decoding entropy using {len(valid_sess)} sessions")
+    decode(valid_sess, args.shuffle_idx)
+
+
+if __name__ == "__main__":
+    main()
