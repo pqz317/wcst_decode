55d16ad185e6ba56d69166920184129813cd83e2
Patrick Zhang <pqz317@uw.edu>
2025-03-09 04:28:11 +0000

    try out multiple sbatches in same script

--- scripts/slurm/20250225_preferred_beliefs_wrapper.sh
+++ scripts/slurm/20250225_preferred_beliefs_wrapper.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 sbatch --array=0-16 <<EOT
 #!/bin/bash
-#SBATCH --job-name=pref_bel
+#SBATCH --job-name=pref
 #SBATCH -p ckpt-all
 #SBATCH -A walkerlab
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --gpus=1
 #SBATCH --mem=32G
 #SBATCH --time=180
 
@@ -15,5 +14,24 @@ singularity exec --writable-tmpfs --nv \
     --bind /gscratch/walkerlab/patrick:/data,/mmfs1/home/pqz317/wcst_decode:/src/wcst_decode \
     /gscratch/walkerlab/patrick/singularity/wcst_decode_image.sif /usr/bin/python3 \
     /src/wcst_decode/scripts/pseudo_decoding/20241113_decode_preferred_beliefs.py \
+    --pair_idx \$SLURM_ARRAY_TASK_ID $1 $2 $3 $4 $5 $6 $7 $8 $9
+EOT
+
+sbatch --array=0-16 <<EOT
+#!/bin/bash
+#SBATCH --job-name=not_pref
+#SBATCH -p ckpt-all
+#SBATCH -A walkerlab
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH --time=180
+
+module load singularity
+singularity exec --writable-tmpfs --nv \
+    --bind /gscratch/walkerlab/patrick:/data,/mmfs1/home/pqz317/wcst_decode:/src/wcst_decode \
+    /gscratch/walkerlab/patrick/singularity/wcst_decode_image.sif /usr/bin/python3 \
+    /src/wcst_decode/scripts/pseudo_decoding/20241113_decode_preferred_beliefs.py \
+    --chosen_not_preferred True
     --pair_idx \$SLURM_ARRAY_TASK_ID $1 $2 $3 $4 $5 $6 $7 $8 $9
 EOT
