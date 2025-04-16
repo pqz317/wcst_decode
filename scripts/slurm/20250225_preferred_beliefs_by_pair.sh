1d02597aa5797f1ea30c25625d28c0ffde7e123c
Patrick Zhang <pqz317@uw.edu>
2025-04-16 23:28:47 +0000

    typo

--- scripts/slurm/20250225_preferred_beliefs_by_pair.sh
+++ scripts/slurm/20250225_preferred_beliefs_by_pair.sh
@@ -13,7 +13,7 @@ module load singularity
 singularity exec --writable-tmpfs --nv \
     --bind /gscratch/walkerlab/patrick:/data,/mmfs1/home/pqz317/wcst_decode:/src/wcst_decode \
     /gscratch/walkerlab/patrick/singularity/wcst_decode_image.sif /usr/bin/python3 \
-    /src/wcst_decode/scripts/pseudo_decoding/20241113_decode_preferred_beliefs.py \
+    /src/wcst_decode/scripts/pseudo_decoding/20241113_decode_preferred_beliefs_by_pairs.py \
     --pair_idx \$SLURM_ARRAY_TASK_ID $1 $2 $3 $4 $5 $6 $7 $8 $9
 EOT
 
@@ -31,7 +31,7 @@ module load singularity
 singularity exec --writable-tmpfs --nv \
     --bind /gscratch/walkerlab/patrick:/data,/mmfs1/home/pqz317/wcst_decode:/src/wcst_decode \
     /gscratch/walkerlab/patrick/singularity/wcst_decode_image.sif /usr/bin/python3 \
-    /src/wcst_decode/scripts/pseudo_decoding/20241113_decode_preferred_beliefs.py \
+    /src/wcst_decode/scripts/pseudo_decoding/20241113_decode_preferred_beliefs_by_pairs.py \
     --chosen_not_preferred True \
     --pair_idx \$SLURM_ARRAY_TASK_ID $1 $2 $3 $4 $5 $6 $7 $8 $9
 EOT
@@ -51,7 +51,7 @@ module load singularity
 singularity exec --writable-tmpfs --nv \
     --bind /gscratch/walkerlab/patrick:/data,/mmfs1/home/pqz317/wcst_decode:/src/wcst_decode \
     /gscratch/walkerlab/patrick/singularity/wcst_decode_image.sif /usr/bin/python3 \
-    /src/wcst_decode/scripts/pseudo_decoding/20241113_decode_preferred_beliefs.py \
+    /src/wcst_decode/scripts/pseudo_decoding/20241113_decode_preferred_beliefs_by_pairs.py \
     --pair_idx \$((\$SLURM_ARRAY_TASK_ID % 17)) \
     --shuffle_idx \$((\$SLURM_ARRAY_TASK_ID / 17)) \
     $1 $2 $3 $4 $5 $6 $7 $8 $9
@@ -72,7 +72,7 @@ module load singularity
 singularity exec --writable-tmpfs --nv \
     --bind /gscratch/walkerlab/patrick:/data,/mmfs1/home/pqz317/wcst_decode:/src/wcst_decode \
     /gscratch/walkerlab/patrick/singularity/wcst_decode_image.sif /usr/bin/python3 \
-    /src/wcst_decode/scripts/pseudo_decoding/20241113_decode_preferred_beliefs.py \
+    /src/wcst_decode/scripts/pseudo_decoding/20241113_decode_preferred_beliefs_by_pairs.py \
     --chosen_not_preferred True \
     --pair_idx \$((\$SLURM_ARRAY_TASK_ID % 17)) \
     --shuffle_idx \$((\$SLURM_ARRAY_TASK_ID / 17)) \
