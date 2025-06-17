f665031ead68ac8e71050dc2dd4b8a6a6974a498
Patrick Zhang <pqz317@uw.edu>
2025-06-17 04:23:22 +0000

    fix

--- scripts/pseudo_decoding/belief_partitions/slurm_launch_choice_reward_int_all.sh
+++ scripts/pseudo_decoding/belief_partitions/slurm_launch_choice_reward_int_all.sh
@@ -3,7 +3,7 @@
 # Default values
 # partition="ckpt-all"
 partition="gpu-a100"
-trial_events="FeedbackOnsetLong"
+trial_event="FeedbackOnsetLong"
 modes="reward_int choice_int updates_beliefs"
 # Optional args passed to decoding script
 extra_args="$@"
